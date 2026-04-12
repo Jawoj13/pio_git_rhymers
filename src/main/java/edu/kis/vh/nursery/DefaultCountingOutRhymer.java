7c8e707f28bebef2fda03565f99b7ec84be234a1
Szymon <255716@edu.p.lodz.pl>
2026-04-12 22:59:59 +0200

    11. i 12. Brak zmian do dodania

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -16,6 +16,7 @@ public class DefaultCountingOutRhymer {
         if (!isFull())
             numbers[++total] = in;
     }
+    
 
         protected boolean callCheck() {
             return total == default_value;
